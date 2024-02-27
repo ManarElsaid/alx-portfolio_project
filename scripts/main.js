7f52bcbd73c62b7cb3f5a8cbb6f488c35730e470
ManarElsaid <elsaidmanar@gmail.com>
2024-02-27 12:30:55 +0200

    java script

---
+++ scripts/main.js
@@ -0,0 +1,50 @@
+const apiKey = "1e427ad5a56d02a3d21d870b54e3bc6a";
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
+const searchBar = document.querySelector(".search input");
+const searchButn = document.querySelector(".search button");
+const weatherIcon = document.querySelector(".weather-icon");
+const findTips = document.querySelector(".tips");
+
+async function checkWeather(cityname){
+    const response = await fetch(apiUrl + cityname + `&appid=${apiKey}`);
+    var weatherData = await response.json();
+
+    document.querySelector(".cityname").innerHTML = weatherData.name;
+    document.querySelector(".tmp").innerHTML = Math.round(weatherData.main.temp) + "°c";
+    document.querySelector(".humdity").innerHTML = weatherData.main.humidity + "%";
+    document.querySelector(".wind").innerHTML = weatherData.wind.speed + " km/h";
+
+    if(weatherData.weather[0].main == "Clouds"){
+        weatherIcon.src = "../images/clouds.png";
+    }
+    else if (weatherData.weather[0].main == "Rain"){
+        weatherIcon.src = "../images/rain.png";
+    }
+    else if (weatherData.weather[0].main == "Mist"){
+        weatherIcon.src = "../images/mist.png";
+    }
+    else if (weatherData.weather[0].main == "Drizzle"){
+        weatherIcon.src = "../images/drizzle.png";
+    }
+    else if (weatherData.weather[0].main == "Clear"){
+        weatherIcon.src = "../images/clear.png";
+    }
+    else if (weatherData.weather[0].main == "Snow"){
+        weatherIcon.src = "../images/snow.png";
+    }
+
+    document.querySelector(".weather").style.display = "block";
+    document.querySelector(".tips").style.display = "block";
+};
+
+searchButn.addEventListener("click", ()=>{
+    checkWeather(searchBar.value);
+});
+
+findTips.addEventListener("click", ()=>{
+    document.querySelector(".profile").style.display = "block";
+})
+
+
+
+
