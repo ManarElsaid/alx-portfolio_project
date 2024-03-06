d7f5c4bfcfef2d2b42aacbd2e63e350c2b6cff98
ManarElsaid <elsaidmanar@gmail.com>
2024-03-06 21:02:44 +0200

    traveller tips

--- scripts/main.js
+++ scripts/main.js
@@ -8,7 +8,7 @@ const topButton = document.querySelector(".top");
 const middleButton = document.querySelector(".middle");
 const bottomButton = document.querySelector(".bottom");
 const momContent = document.querySelector(".mom p");
-const maleContent = document.querySelector(".male p");
+const travContent = document.querySelector(".traveller p");
 const femaleContent = document.querySelector(".female p");
 
 async function checkWeather(cityname){
@@ -29,6 +29,12 @@ async function checkWeather(cityname){
         <li>Plan indoor activities like reading, puzzles, or movie time for a cozy day at home.</li>\
         <li>Use this weather as an opportunity to talk about cloud formations and weather patterns with your child.</li>\
     </ul>";
+
+        travContent.innerHTML = "<ul> \
+        <li>Bring along a light jacket or sweater for cooler temperatures.</li>\
+        <li>Consider indoor attractions like shopping malls, theaters, or indoor markets.</li>\
+        <li>Embrace the moody ambiance by exploring historic sites or quaint villages.</li>\
+    </ul>";
     }
     else if (weatherData.weather[0].main == "Rain"){
         weatherIcon.src = "../images/rain.png";
@@ -39,8 +45,12 @@ async function checkWeather(cityname){
         <li>Plan indoor activities like board games, arts and crafts, or baking to keep your child entertained.</li>\
         <li>Check for any potential leaks or flooding around your home and take necessary precautions to keep your child safe and dry.</li>\
     </ul>";
-        femaleContent.innerHTML = "Stay dry without sacrificing style by opting for a chic trench coat or a trendy waterproof jacket. Pair it with stylish rain boots and a cute umbrella to complete your rainy day ensemble.";
-        maleContent.innerHTML = "Stay dry in style with a waterproof jacket or coat. Opt for a versatile trench coat or a sporty rain jacket paired with waterproof shoes to keep your feet dry and comfortable.";
+    
+        travContent.innerHTML = "<ul> \
+        <li>Pack a compact umbrella or waterproof raincoat to stay dry while exploring.</li>\
+        <li>Wear waterproof shoes to avoid soggy feet while walking around.</li>\
+        <li>Plan indoor activities like visiting museums, galleries, or cafes to escape the rain.</li>\
+    </ul>";
     }
     else if (weatherData.weather[0].main == "Mist"){
         weatherIcon.src = "../images/mist.png";
@@ -51,6 +61,12 @@ async function checkWeather(cityname){
         <li>Be cautious while driving and use fog lights if visibility is low.</li>\
         <li>If going outdoors, stay on well-marked trails and paths to avoid getting lost in the mist.</li>\
     </ul>";
+
+        travContent.innerHTML = "<ul> \
+        <li>Be cautious while driving and use fog lights if visibility is reduced.</li>\
+        <li>Allow extra time for travel and use a map or GPS to navigate safely.</li>\
+        <li>Enjoy the mystical atmosphere by visiting scenic spots or taking nature walks in the mist.</li>\
+    </ul>";
     }
     else if (weatherData.weather[0].main == "Drizzle"){
         weatherIcon.src = "../images/drizzle.png";
@@ -60,6 +76,12 @@ async function checkWeather(cityname){
         <li>Dress your child in waterproof clothing and pack extra layers to stay warm.</li>\
         <li>Choose indoor activities like indoor play centers, museum visits, or movie marathons.</li>\
         <li>If going outdoors, avoid slippery surfaces and watch out for puddles to prevent accidents.</li>\
+    </ul>";
+
+        travContent.innerHTML = "<ul> \
+        <li>Pack a compact umbrella or a waterproof jacket with a hood.</li>\
+        <li>Explore covered outdoor attractions like botanical gardens or covered markets.</li>\
+        <li>Take breaks in cafes or restaurants to warm up and enjoy hot beverages.</li>\
     </ul>";
     }
     else if (weatherData.weather[0].main == "Clear"){
@@ -72,8 +94,12 @@ async function checkWeather(cityname){
             <li>Remember to apply sunscreen to protect your child's skin from harmful UV rays.</li>\
             <li>Keep your child hydrated by offering plenty of water,<br>especially if they're playing outside.</li>\
         </ul>"
-        femaleContent.innerHTML = " Showcase your style with breezy dresses and skirts in lightweight fabrics like chiffon or silk. Pair them with open-toe shoes or sandals for an effortlessly chic look.";
-        maleContent.innerHTML = " Keep it cool and casual with breathable fabrics like cotton or linen. Pair a classic button-down shirt with chinos or shorts for a laid-back yet polished look.";
+    
+        travContent.innerHTML = "<ul> \
+        <li>Pack sunglasses and sunscreen to protect yourself from the sun's rays.</li>\
+        <li>Stay hydrated by carrying a water bottle and drinking plenty of fluids,<br>especially if you're exploring outdoors.</li>\
+        <li>Take advantage of the clear skies for outdoor sightseeing and photography.</li>\
+    </ul>"
     }
     else if (weatherData.weather[0].main == "Snow"){
         weatherIcon.src = "../images/snow.png";
@@ -81,8 +107,14 @@ async function checkWeather(cityname){
         //changing tips for snowy days
         momContent.innerHTML = "<ul> \
         <li>Dress your child in warm, waterproof clothing, including snow pants, boots, gloves, and hats.</li>\
-        <li>.Build a snowman, have a snowball fight, or go sledding for some fun outdoor activities.</li>\
+        <li>Build a snowman, have a snowball fight, or go sledding for some fun outdoor activities.</li>\
         <li>Check road conditions and plan your outings accordingly to ensure safe travels.</li>\
+    </ul>"
+
+        travContent.innerHTML = "<ul> \
+        <li>Dress in layers with waterproof and insulated clothing to stay warm.</li>\
+        <li>Wear sturdy boots with good traction to prevent slipping on icy sidewalks.</li>\
+        <li>Enjoy winter activities like skiing, snowboarding, or building snowmen in designated areas.</li>\
     </ul>"
     }
 
@@ -103,13 +135,13 @@ findTips.addEventListener("click", ()=>{
 topButton.addEventListener("click", ()=>{
     document.querySelector(".mom").style.display = "flex";
     document.querySelector(".mom").style.transitionDuration = "2s";
-    document.querySelector(".male").style.display = "none";
+    document.querySelector(".traveller").style.display = "none";
     document.querySelector(".female").style.display = "none";
 
 });
 
 middleButton.addEventListener("click", ()=>{
-    document.querySelector(".male").style.display = "block";
+    document.querySelector(".traveller").style.display = "block";
     document.querySelector(".mom").style.display = "none";
     document.querySelector(".female").style.display = "none";
 });
@@ -117,7 +149,7 @@ middleButton.addEventListener("click", ()=>{
 bottomButton.addEventListener("click", ()=>{
     document.querySelector(".female").style.display = "block";
     document.querySelector(".mom").style.display = "none";
-    document.querySelector(".male").style.display = "none";
+    document.querySelector(".traveller").style.display = "none";
 });
 
 
